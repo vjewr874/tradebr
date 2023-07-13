59ba8b4a508a30dd2698befdaaf43f4fa75f5b93
vjewr874 <contact@hungbe.dev>
2023-07-13 02:15:03 -0700

    first commit

---
+++ 06_bizzan_client_android/app/src/main/java/cn/bizzan/config/UrlConfig.java
@@ -0,0 +1,11 @@
+package cn.bizzan.config;
+
+public class UrlConfig {
+
+    public static String BASE_URL="https://api.btxchain.com";
+    public static String C2C_URL="api.btxchain.com";
+    public static String MARKET_URL="api.btxchain.com";
+    public static String GROUP_URL="api.btxchain.com";
+    public static String HEART_URL="api.btxchain.com";
+
+}
